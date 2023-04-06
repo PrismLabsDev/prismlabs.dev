8855a24705af8bf23e5a21c95a2a43ce208d61f2
Jack Woodrow <jackwoodrow99@gmail.com>
2023-04-06 15:27:28 -0400

    Base site opperation and blog

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				lato: ['Lato', 'sans-serif'],
+				montserrat: ['Montserrat', 'sans-serif'],
+				opensans: ['Open Sans', 'sans-serif'],
+				poppins: ['Poppins', 'sans-serif'],
+				righteous: ['Righteous', 'cursive'],
+				roboto: ['Roboto', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
-}
+};
